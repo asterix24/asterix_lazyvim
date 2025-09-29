b4f89945d2b3ba715c5f84ad8b01f5fc24c83503
Daniele Basile <asterix@develer.com>
2025-09-29 23:01:43 +0200

    remap mini move keybind

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -51,3 +51,19 @@ require("lazy").setup({
     },
   },
 })
+
+-- Configure mini-move keymapping
+-- Shift + arrows
+require("mini.move").setup({
+  mappings = {
+    left = "<S-left>",
+    right = "<S-right>",
+    down = "<S-down>",
+    up = "<S-up>",
+
+    line_left = "<S-left>",
+    line_right = "<S-right>",
+    line_down = "<S-down>",
+    line_up = "<S-up>",
+  },
+})
